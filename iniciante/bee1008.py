7ac43c56ee848530d94bc618ea1e2a8767190e48
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-05-11 21:10:20 -0300

    BEE 1008

---
+++ iniciante/bee1008.py
@@ -0,0 +1,8 @@
+funcionario = int(input())
+horas_trabalhadas = int(input())
+valor_hora = float(input())
+
+salario = valor_hora * horas_trabalhadas
+
+print(f"NUMBER = {funcionario}")
+print(f"SALARY = U$ {salario:.2f}")
