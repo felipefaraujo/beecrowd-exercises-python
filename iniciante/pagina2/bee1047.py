44478610f63f62daf57a4b3dd97bf180181bf6d4
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-05-16 17:08:10 -0300

    Página 2 - Iniciante

---
+++ iniciante/pagina2/bee1047.py
@@ -0,0 +1,14 @@
+hora_inicial, minuto_inicial, hora_final, minuto_final = map(int, input().split())
+
+total_inicial = hora_inicial * 60 + minuto_inicial
+total_final = hora_final * 60 + minuto_final
+
+tempo = total_final - total_inicial
+
+if tempo == 0:
+    tempo += 1440
+
+horas = tempo // 60
+minutos = tempo % 60
+
+print(f"O JOGO DUROU {horas} HORA(S) E {minutos} MINUTO(S)")
