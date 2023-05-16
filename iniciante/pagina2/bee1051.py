932509b9f114ad45e158988564778e48d96c2eda
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-05-16 18:49:44 -0300

    Página 2 - Iniciante

---
+++ iniciante/pagina2/bee1051.py
@@ -0,0 +1,19 @@
+salario = float(input())
+
+if 0.00 <= salario <= 2000.00:
+    print("Isento")
+elif 2000.01 <= salario <= 3000.00:
+    imposto = (salario - 2000.00) * 0.08
+    print(f"R$ {imposto:.2f}")
+elif 3000.01 <= salario <= 4500.00:
+    imposto = salario - 2000.00
+    if imposto > 1000.00:
+        imposto = (imposto - 1000.00) * 0.18
+        imposto += 80
+        print(f"R$ {imposto:.2f}")
+    else:
+        imposto = (salario - 2000.00) * 0.18
+        print(f"R$ {imposto:.2f}")
+elif salario > 4500.00:
+    imposto = ((salario - 4500.00) * 0.28) + 80.00 + 270.00
+    print(f"R$ {imposto:.2f}")
