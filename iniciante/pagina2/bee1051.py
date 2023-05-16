2644464c100180f22490cf8fba192f6ccd0a7b06
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-05-16 18:55:50 -0300

    Página 2 - Iniciante

--- iniciante/pagina2/bee1051.py
+++ iniciante/pagina2/bee1051.py
@@ -6,14 +6,8 @@ elif 2000.01 <= salario <= 3000.00:
     imposto = (salario - 2000.00) * 0.08
     print(f"R$ {imposto:.2f}")
 elif 3000.01 <= salario <= 4500.00:
-    imposto = salario - 2000.00
-    if imposto > 1000.00:
-        imposto = (imposto - 1000.00) * 0.18
-        imposto += 80
-        print(f"R$ {imposto:.2f}")
-    else:
-        imposto = (salario - 2000.00) * 0.18
-        print(f"R$ {imposto:.2f}")
+    imposto = ((salario - 3000.00) * 0.18) + 80.00
+    print(f"R$ {imposto:.2f}")
 elif salario > 4500.00:
     imposto = ((salario - 4500.00) * 0.28) + 80.00 + 270.00
     print(f"R$ {imposto:.2f}")
