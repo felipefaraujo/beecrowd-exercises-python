491461a2bc3c585734537af1f171fe7e9deda24e
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-07-07 17:10:22 -0300

    Página 4 - Iniciante

--- iniciante/pagina4/bee1151.py
+++ iniciante/pagina4/bee1151.py
@@ -1,23 +1,13 @@
-def fibonacci(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
+n = int(input())
+a = 0
+b = 1
+
+for i in range(n):
+    if i == n-1:
+        print(a)
     else:
-        sequence = [0, 1]
+        print(a, end=" ")
 
-        while len(sequence) < n:
-            next_number = sequence[-1] + sequence[-2]
-            sequence.append(next_number)
-
-        return sequence
-
-
-num = int(input())
-
-fib_sequence = fibonacci(num)
-
-saida = " ".join(map(str, fib_sequence))
-print(saida)
+    c = a + b
+    a = b
+    b = c
---
+++ iniciante/pagina4/bee1153.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+fatorial = 1
+for i in range(n, 0, -1):
+    fatorial *= i
+
+print(fatorial)
