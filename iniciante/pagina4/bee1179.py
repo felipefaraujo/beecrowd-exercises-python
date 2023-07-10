4fabb98b886e49d89bde1362e2c17aeb24c2ba4c
Felipe F Araujo <devfelipefaraujo@gmail.com>
2023-07-10 19:49:19 -0300

    Update bee1179.py

--- iniciante/pagina4/bee1179.py
+++ iniciante/pagina4/bee1179.py
@@ -10,18 +10,18 @@ for i in range(15):
 
 contador_par = 0
 contador_impar = 0
+
 for j, valor in enumerate(vetor):
-    if valor % 2 == 0:
+    if valor % 2 == 0 and contador_par < 5:
         par.append(valor)
         contador_par += 1
-        if contador_par == 5:
-            par2.append(valor)
-
-    elif valor % 2 != 0:
+    elif valor % 2 == 0 and contador_par >= 5:
+        par2.append(valor)
+    elif valor % 2 != 0 and contador_impar < 5:
         impar.append(valor)
         contador_impar += 1
-        if contador_impar == 5:
-            impar2.append(valor)
+    elif valor % 2 != 0 and contador_impar >= 5:
+        impar2.append(valor)
 
 for x, valor in enumerate(par):
     print(f"par[{x}] = {valor}")
@@ -29,8 +29,8 @@ for x, valor in enumerate(par):
 for w, valor in enumerate(impar):
     print(f"impar[{w}] = {valor}")
 
-for y, valor in enumerate(par2):
-    print(f"par[{y}] = {valor}")
-
 for z, valor in enumerate(impar2):
     print(f"impar[{z}] = {valor}")
+
+for y, valor in enumerate(par2):
+    print(f"par[{y}] = {valor}")
